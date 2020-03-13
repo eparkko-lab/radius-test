7eb4f3c0e34202b822423c2be8255385c1c07cb7
eparkko-lab <erik.parkkonen@yubico.com>
2020-03-13 09:35:04 -0700

    added usage

--- rad.sh
+++ rad.sh
@@ -5,6 +5,20 @@ USER_NAME=$2
 CODE=$3
 ENDPOINT=$4
 
+usage()
+{
+    echo ""
+    echo "Usage:"
+    echo "./rad.sh 'SHARED_SECRET' 'USER_NAME' 'OTP_CODE' '['CAS' | 'AM']'"    
+}
+
+
+if [ -z $SHARED_SECRET ] || [ -z $USER_NAME ] || [ -z $CODE ]
+then 
+    usage
+    exit 1
+fi
+
 if [ -z $ENDPOINT ]
 then
     ENDPOINT='10.0.0.8'
@@ -19,5 +33,7 @@ if [ $ENDPOINT = 'AM' ]
 then
     ENDPOINT='10.0.0.21'
 fi
+
+
 
 radtest "$USER_NAME" "$CODE" "$ENDPOINT" 0 "$SHARED_SECRET"
